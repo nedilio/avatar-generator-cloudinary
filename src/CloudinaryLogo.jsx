502d2d9b2b837496c166f7635e40fda744a5f0bb
Nelson Izquierdo <izquierdonelson@gmail.com>
2023-02-22 01:20:36 -0300

    fix: class warning

--- src/CloudinaryLogo.jsx
+++ src/CloudinaryLogo.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const CloudinaryLogo = () => {
   return (
     <svg
-      class="w-36"
+      className="w-36"
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 500 96.77"
     >

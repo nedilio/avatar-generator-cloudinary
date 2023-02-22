2411b6c693876e4a44b4c2a7ea84fc3e18b9a5a2
Nelson Izquierdo <izquierdonelson@gmail.com>
2023-02-22 00:25:20 -0300

    text and styles change

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,17 @@ function App() {
   return (
     <div className="max-w-lg mx-auto text-center p-4 text-gray-800">
       <div className="flex flex-col gap-4">
-        <h1 className="font-bold text-3xl">Cloudinary test</h1>
+        <h1 className="font-bold text-6xl">
+          Avatar<span className="text-purple-500 text-6xl">Generator</span>
+        </h1>
+        <a
+          href="https://github.com/nedilio"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="font-semibold text-purple-500 hover:underline"
+        >
+          by Nedilio
+        </a>
         <UploadStep />
       </div>
     </div>

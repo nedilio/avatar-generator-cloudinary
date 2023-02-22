f785e849b6869b5caaf41ad00b9b17a089cf2666
Nelson Izquierdo <izquierdonelson@gmail.com>
2023-02-22 00:35:56 -0300

    fix(styles): title size on mobile devices

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,11 @@ function App() {
   return (
     <div className="max-w-lg mx-auto text-center p-4 text-gray-800">
       <div className="flex flex-col gap-4">
-        <h1 className="font-bold text-6xl">
-          Avatar<span className="text-purple-500 text-6xl">Generator</span>
+        <h1 className="font-bold text-4xl md:text-6xl">
+          Avatar
+          <span className="text-purple-500 text-4xl md:text-6xl">
+            Generator
+          </span>
         </h1>
         <a
           href="https://github.com/nedilio"

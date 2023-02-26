fef7a43c8ea913a195a6cc32dc021d94fc22022c
Nelson Izquierdo <izquierdonelson@gmail.com>
2023-02-26 00:33:07 -0300

    fix: optimize svg icons

--- src/components/icons/BackIcon.jsx
+++ src/components/icons/BackIcon.jsx
@@ -1,20 +1,14 @@
 import React from "react";
 
-const BackIcon = () => {
+const BackIcon = (props) => {
   return (
-    <svg
-      height="21"
-      viewBox="0 0 21 21"
-      width="21"
-      xmlns="http://www.w3.org/2000/svg"
-    >
+    <svg height={21} width={21} xmlns="http://www.w3.org/2000/svg" {...props}>
       <path
-        d="m5.66116524 3.36827202c5.18469776-.47094658 8.51890836 1.5289737 9.99999996 6-2.8248102-3.14044041-6.34158528-3.71816233-9.99999996-2v2.99999998l-5-4.99999998 5-5z"
+        d="M8.5 7.5c5.185-.47 8.52 1.53 10 6-2.825-3.14-6.341-3.718-10-2v3l-5-5 5-5z"
         fill="none"
         stroke="currentColor"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        transform="translate(2.839 4.132)"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
--- src/components/icons/DownloadIcon.jsx
+++ src/components/icons/DownloadIcon.jsx
@@ -1,24 +1,17 @@
 import React from "react";
 
-const DownloadIcon = () => {
+const DownloadIcon = (props) => {
   return (
-    <svg
-      height="21"
-      viewBox="0 0 21 21"
-      width="21"
-      xmlns="http://www.w3.org/2000/svg"
-    >
+    <svg height={21} width={21} xmlns="http://www.w3.org/2000/svg" {...props}>
       <g
         fill="none"
-        fill-rule="evenodd"
+        fillRule="evenodd"
         stroke="currentColor"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        transform="translate(4 2)"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       >
-        <path d="m4.5 1.5h-2.00245461c-1.1045695 0-2 .8954305-2 2v10c0 1.1045695.8954305 2 2 2h8.00000001c1.1043778-.000491 1.9997288-.8956223 2.0004909-2l.0019637-8-4-4" />
-        <path d="m9.5 8.586-3 2.914-3-2.914" />
-        <path d="m6.5.5v11" />
+        <path d="M8.5 3.5H6.498a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h8c1.104 0 2-.896 2-2l.002-8-4-4" />
+        <path d="m13.5 10.586-3 2.914-3-2.914M10.5 2.5v11" />
       </g>
     </svg>
   );
